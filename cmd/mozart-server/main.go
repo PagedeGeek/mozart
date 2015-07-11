d42378a2fe5cf18618d910038c30185bb6bdfb60
Sam <samuel@pagedegeek.com>
2015-07-11 16:08:35 +0200

    Update directories

---
+++ cmd/mozart-server/main.go
@@ -0,0 +1,20 @@
+package main
+
+import "github.com/pagedegeek/mozart"
+
+func main() {
+
+	// TODO: load json file
+	jsonConfig := `{
+                "listen_info": "localhost:1357"
+        }`
+
+	if config, err := mozart.NewConfigFromJSON([]byte(jsonConfig)); err != nil {
+		panic(err)
+	} else {
+		s := mozart.NewService(config)
+		if err := s.Start(); err != nil {
+			panic(err)
+		}
+	}
+}
